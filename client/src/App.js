277c523584e40b95b2ef34e5c035b1f553a9592f
karla <karla@getharvest.com>
2021-12-21 22:30:25 -0800

    Set up basic app.js and index.js files

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable import/no-anonymous-default-export */
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import Courses from './components/Courses';
+import withContext from './Context';
+
+const CoursesWithContext = withContext(Courses);
+
+
+export default () => (
+  <Router>
+    <div>
+      <Switch>
+        <Route path="/courses" component={CoursesWithContext} />
+      </Switch>
     </div>
-  );
-}
+  </Router>
+);
 
-export default App;
