a8622d3fa276b20830c38d9a5d3825e0f4ccfc27
karla <karla@getharvest.com>
2021-12-22 15:17:16 -0800

    Add Header component

--- client/src/App.js
+++ client/src/App.js
@@ -7,23 +7,27 @@ import {
   Redirect
 } from 'react-router-dom';
 
+import Header from './components/Header';
 import Courses from './components/Courses';
 import CourseDetail from './components/CourseDetail';
 import withContext from './Context';
 
+const HeaderWithContext = withContext(Header);
 const CoursesWithContext = withContext(Courses);
 const CourseDetailWithContext = withContext(CourseDetail);
 
 
 export default () => (
   <Router>
-    <div>
+  
+      <HeaderWithContext />
+
       <Switch>
         <Redirect exact from='/' to='/courses' />
         <Route exact path="/courses" component={CoursesWithContext} />
         <Route path="/courses/:id" component={CourseDetailWithContext} />
       </Switch>
-    </div>
+
   </Router>
 );
 
---
+++ client/src/components/Header.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = ({context}) => {
+  const authUser = context.authenticatedUser;
+  
+  return (
+
+    <header>
+    <div className="wrap header--flex">
+        <h1 className="header--logo"><a href="/">Courses</a></h1>
+        <nav>
+        {authUser ?
+          <ul className="header--signedin">
+              <li>Welcome, {authUser.name}!</li>
+              <li><a href="/signout">Sign Out</a></li>
+          </ul>
+          :
+          <ul className="header--signedout">
+              <li><a href="/signup">Sign Up</a></li>
+              <li><a href="/signin">Sign In</a></li>
+          </ul>
+          }
+        </nav>
+    </div>
+</header>
+  );
+
+}
+
+export default Header;
